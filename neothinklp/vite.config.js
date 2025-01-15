af5e2f1406d19ff03884b1c6cb82ffd2a67fee60
Chirag <167162461+chintaal@users.noreply.github.com>
2025-01-16 00:16:39 +0530

    nice

--- neothinklp/vite.config.js
+++ neothinklp/vite.config.js
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+    }),
+    paths: {
+      base: '/neothink-landingpage',
+    },
+  }
 });
